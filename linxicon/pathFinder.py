05f9737a120a250afbe3a1f99caaf1f0c41fc5df
Jamie Bali <92593423+JamieBali@users.noreply.github.com>
2026-07-06 17:00:48 +0100

    Update pathFinder.py
    error catching and fixing bugs where it required variable type checkers and output in an irregular format.

--- linxicon/pathFinder.py
+++ linxicon/pathFinder.py
@@ -3,15 +3,32 @@ import numpy as np
 
 
 def find_path(starting_word, ending_word, mapping):
-    routes = [starting_word]
 
-    while True:
+    # first confirm both words are in the list
+    try:
+        _ = mapping[starting_word]
+    except:
+        print("selected STARTING word not included in dictionary")
+        return False
+    try:
+        _ = mapping[ending_word]
+    except:
+        print("selected ENDING word not included in dictionary")
+        return False
+
+    # then do a 0th level, appending the first set into a list
+    initials = mapping[starting_word]
+    routes = []
+    for word in initials:
+        if word == ending_word:
+            return [starting_word, word]
+        routes.append([starting_word, word])
+
+    # then recurse. We'll do a maximum of 10 recursions before calling it quits
+    for _ in range (0,10):
         temp_routes = []
         for route in routes:
-            if type(route) == type(str("str")):
-                word_to_check = route
-            else:
-                word_to_check = route[-1]
+            word_to_check = route[-1]
             outward_paths = mapping[word_to_check]
             for word in outward_paths:
 
@@ -19,19 +36,17 @@ def find_path(starting_word, ending_word, mapping):
                     route.append(word)
                     return route
                 
-                else:
-                    if type(route) == type(str("str")):
-                        temp = [route]
-                    else:
-                        temp = list(np.copy(route))
-                    temp.append(word)
+                temp = []
+                for item in route:
+                    temp.append(item)
+                temp.append(word)
                 temp_routes.append(temp)
         routes = temp_routes
+
+    print("unable to find a route")
+    return False
 
 with open('saved_dictionary.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
 
-w1 = input("starting word: ")
-w2 = input("ending word: ")
-
-print(find_path(w1,w2,loaded_dict))
+print(find_path("find","cat",loaded_dict))
